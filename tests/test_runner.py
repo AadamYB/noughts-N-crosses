a6b232cc52a6c668940d0348cb25ab90c41b8faa
Aadam <ec221024@qmul.ac.uk>
2025-05-04 15:53:34 +0100

    What will happen???

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,10 +1,11 @@
 """test scripts"""
 
 import pytest
-from app import main
+from tkinter import *
+from app import noughtsNcrosses
 
 
-def test_exxample():
+def test_example():
     """just assert true"""
     assert True
 
@@ -13,6 +14,14 @@ def test_success():
     """this should pass"""
     assert 1 == 1
 
+def test_initial_player_label():
+    root = Tk()
+    root.withdraw()  # Prevents the game window from showing during test
+    game = noughtsNcrosses(root)
+
+    label_text = game.label.cget("text")
+    assert label_text in ["X's Turn", "O's Turn"]
+    root.destroy()
 
 # def test_failure():
 #     """this should fail"""
